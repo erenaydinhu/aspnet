1d8e61f3d92509b9685b3e5f558c9e9e79d84cda
Your Name <you@example.com>
2016-06-25 23:24:32 +0200

    first

---
+++ WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+using System.Xml.Linq;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        XElement svg = new XElement("svg", Request.QueryString["fname"]);
+
+        XDocument SVG = new XDocument();
+        SVG.Add(svg);
+
+
+        var SVGDX = new XAttribute("x", "0");
+        var SVGDY = new XAttribute("y", "15");
+        svg.Add(SVGDX);
+        svg.Add(SVGDY);
+
+
+        SVG.Save(@"C:\Users\Eren\Desktop\sss.xml");
+
+
+    }
+}
---
+++ WebApplication1/obj/Release/Package/PackageTmp/WebForm1.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="WebForm1.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+
+
+    
+    <div>
+    <svg>
+  <text x="0" y="15" > <%= Request.QueryString["fname"] %>  </text>
+   </svg>
+    </div>
+
+</body>
+</html>
